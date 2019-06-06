c72a308c9f2cb7201499d0565ea0f3ae82d32612
徐星晨 <xuxingchen@hinterstellar.com>
2019-06-06 11:36:16 +0800

    add global config

--- app/src/main/java/com/xxc/dev/main/base/BaseBottomTabActivity.java
+++ app/src/main/java/com/xxc/dev/main/base/BaseBottomTabActivity.java
@@ -1,10 +1,22 @@
 package com.xxc.dev.main.base;
 
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import com.xxc.dev.main.R;
+
 /**
  * 底部带有tab的activity
  */
 public abstract class BaseBottomTabActivity extends AbsActivity {
 
 
+    @Override
+    protected int getContentId() {
+        return R.layout.activity_bottom_tab;
+    }
 
+    @Override
+    protected void initWidgets(Bundle savedInstanceState) {
+
+    }
 }
